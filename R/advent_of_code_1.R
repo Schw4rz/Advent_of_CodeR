bfc4179e90e580dcd742c49d39a34a62f44e2ddc
Schw4rz <schwarz.sebastian.ss@googlemail.com>
2017-12-05 23:44:30 +0100

    split functions

--- R/advent_of_code_1.R
+++ R/advent_of_code_1.R
@@ -1,7 +1,21 @@
-#' Advent of Code 1 - Sum by Match
+#' Read String Data to Vector
 #'
-#' @param input [\code{character(1)}]\cr
-#'   input as single character.
+#' @param path [\code{character(1)}]\cr
+#'   path to the file.
+#'
+#' @return [\code{vector}]\cr 
+#'   returns data in as vector of single numbers.
+#' @export
+#'
+#' @examples readDataVec("data/advent_of_code_1.txt")
+readDataVec <- function(path) {
+  as.numeric(strsplit(readLines(path), "")[[1]])
+}
+
+#' Sum by Match
+#'
+#' @param in_vec [\code{numeric}]\cr
+#'   numeric vector.
 #' @param distance [\code{integer(1)}]\cr
 #'   distance to compare with.
 #' @return [\code{integer(1)}]\cr
@@ -10,18 +24,17 @@
 #' @export
 #'
 #' @examples calculateSumByMatch("1221", 1)
-calculateSumByMatch <- function(input, distance) {
-  in_vec <- as.numeric(strsplit(input, "")[[1]])
+calculateSumByMatch <- function(in_vec, distance) {
   pad_vec_idx <- seq_len(distance)
   in_vec_lag <- c(in_vec[-pad_vec_idx], in_vec[pad_vec_idx])
   sum(in_vec[in_vec == in_vec_lag])
 }
 
 # calcuate results ------------------------------------------------------------
-input <- readLines("data/advent_of_code_1.txt")
+in_vec <- readDataVec("data/advent_of_code_1.txt")
 
 # part 1
-calculateSumByMatch(input, 1)
+calculateSumByMatch(in_vec, 1)
 
 # part 2
-calculateSumByMatch(input, nchar(input) / 2)
+calculateSumByMatch(in_vec, length(in_vec) / 2)
